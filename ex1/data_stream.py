d1bea9df629c8382c0b853b5c6ec8442d184be45
Kyrylo Volynskyi <kvolynsk@f1r1s4.codam.nl>
2026-02-03 20:01:48 +0100

    wip

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -1,8 +1,39 @@
 from abc import ABC, abstractmethod
-from typing import List, Any
+from typing import List, Any, Optional, Union, Dict
 
 
 class DataStream(ABC):
+    def __init__(self, stream_id: str):
+        self.stream_id = stream_id
+
     @abstractmethod
     def process_batch(self, data_batch: List[Any]) -> str:
         pass
+
+    def filter_data(
+        self, data_batch: List[Any], criteria: Optional[str] = None
+    ) -> List[Any]:
+        return data_batch
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
+
+
+class SensorStream(DataStream):
+    pass
+
+
+class TransactionStream(DataStream):
+    pass
+
+
+class EventStream(DataStream):
+    pass
+
+
+class StreamProcessor:
+    pass
+
+
+if __name__ == "__main__":
+    print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===")
